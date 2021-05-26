b32d870472b5b1f79567c00cc4e3a1fd43e7d0d4
Markus Thulin <macky_b@hotmail.com>
2021-05-26 22:23:15 +0200

    Add server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const hbs = require('hbs');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({ path: './config/config.env' });
+
+hbs.registerHelper("prettifyDate", function(timestamp) {
+    const options = {year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(timestamp*1000).toLocaleDateString('en-GB', options);
+});
+
+hbs.registerHelper("isNotNull", function(value) {
+    return value !== null;
+});
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/', require('./routes/index'));
+
+const PORT = process.env.PORT || 4567;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
