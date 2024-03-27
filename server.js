80f049c31374ade655cde48c8134a2c1968f505f
Markus Thulin <macky_b@hotmail.com>
2024-03-27 13:11:39 +0100

    Added apicache to cache calls to api for 1 hour

--- server.js
+++ server.js
@@ -1,35 +1,40 @@
-const express = require('express');
-const cors = require('cors');
-const hbs = require('hbs');
-const dotenv = require('dotenv');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const hbs = require("hbs");
+const dotenv = require("dotenv");
+const path = require("path");
+const apicache = require("apicache");
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
-hbs.registerHelper("prettifyDate", function(timestamp) {
-    const options = {year: 'numeric', month: 'long', day: 'numeric' };
-    return new Date(timestamp*1000).toLocaleDateString('en-GB', options);
+hbs.registerHelper("prettifyDate", function (timestamp) {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(timestamp * 1000).toLocaleDateString("en-GB", options);
 });
 
-hbs.registerHelper("isNotNull", function(value) {
+hbs.registerHelper("isNotNull", function (value) {
     return value !== null;
 });
 
-hbs.registerHelper('limit', function (arr, limit) {
-    if (!Array.isArray(arr)) { return []; }
+hbs.registerHelper("limit", function (arr, limit) {
+    if (!Array.isArray(arr)) {
+        return [];
+    }
     return arr.slice(0, limit);
 });
 
 const app = express();
+const cache = apicache.middleware;
 
 //Middleware
 app.use(cors());
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.use(cache("1 hour"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.use('/', require('./routes/index'));
+app.use("/", require("./routes/index"));
 
 const PORT = process.env.PORT || 4567;
 
